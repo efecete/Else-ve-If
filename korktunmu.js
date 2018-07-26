ba98f7d5b895def2c08fbe3040f323354aac1e30
efecete <41728127+efecete@users.noreply.github.com>
2018-07-26 12:03:21 +0300

    Add files via upload

---
+++ korktunmu.js
@@ -0,0 +1,23 @@
+var yilan_korktunmu = false;
+var orumcek_korktunmu = true;
+var Wraaaaaaaah_korktunmu = true;
+
+if(yilan_korktunmu)                                            //else if ve if komutları yandaki gibi sıralandığında birlerşiktir ancak sadece if kullanırsa bilgisayar tek tek komutlara bakar
+{
+    console.log(" ");
+    console.log ("yılanlar yılanlardır");
+    console.log(" ");
+}
+else if(orumcek_korktunmu)
+{   
+    console.log(" ");
+    console.log("örümcekler ömürceklerdir, ömürlerimizi yerler");
+    console.log(" ");
+}
+else if(Wraaaaaaaah_korktunmu)
+{
+    console.log(" ");
+    console.log("bi bundan korkmadığın kalmıştı zaten");
+    console.log(" ");
+
+}
